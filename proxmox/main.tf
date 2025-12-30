15e94997f8c04f1391ca7afcfc88e175b1c934d5
michielvha <michielvh@outlook.com>
2025-12-30 03:27:12 +0100

    fix: issues with output

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -1,7 +1,7 @@
-# Simple data source to test Proxmox connectivity
-data "proxmox_virtual_environment_nodes" "nodes" {}
+# Simple data source to test Proxmox connectivity - using version for simplicity
+data "proxmox_virtual_environment_version" "version" {}
 
-# Output the node names to verify it works
-output "proxmox_nodes" {
-  value = data.proxmox_virtual_environment_nodes.nodes.node_names
+# Output the version to verify it works
+output "proxmox_version" {
+  value = data.proxmox_virtual_environment_version.version
 }
